3455ab7724bd58972fb41508e288d70b68ee741a
Arhinful Emmanuel <kofibusy@gmail.com>
2023-06-19 14:10:50 +0000

    add sluggable options

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Sluggable\SlugOptions;
 
 class User extends Authenticatable
 {
@@ -31,5 +32,12 @@ class User extends Authenticatable
 
     public function tasks(): HasMany{
         return $this->hasMany(Task::class);
+    }
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
     }
 }
