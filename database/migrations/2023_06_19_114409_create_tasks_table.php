d84b587413aac05e346f24cf02bb1703e44fad55
Arhinful Emmanuel <kofibusy@gmail.com>
2023-06-19 12:17:42 +0000

    updated task migration file

--- database/migrations/2023_06_19_114409_create_tasks_table.php
+++ database/migrations/2023_06_19_114409_create_tasks_table.php
@@ -18,7 +18,8 @@ return new class extends Migration
             $table->string('slug')->unique();
             $table->string('task');
             $table->mediumText('description')->nullable();
-            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('delegate_id')->nullable()->constrained('users')->nullOnDelete();
 
             $table->boolean('is_active')->default(true);
             $table->foreignId('added_by_id')->nullable();
