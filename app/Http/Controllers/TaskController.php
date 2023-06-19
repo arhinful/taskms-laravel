ab8e9e5abd6d0509499a50d85b8210bcb9c1d16b
Arhinful Emmanuel <kofibusy@gmail.com>
2023-06-19 12:06:47 +0000

    work on task index

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use Spatie\QueryBuilder\QueryBuilder;
+
 class TaskController extends Controller
 {
-
+    public function index(){
+        $tasks = QueryBuilder::for(Task::class)
+            ->allowedFilters([
+                "task",
+                "description",
+                "user_id",
+            ])
+            ->paginate();
+    }
 }
