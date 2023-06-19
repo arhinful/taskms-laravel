de9af7dcc9735c3eaf13c4b941fad5982bb4b086
Arhinful Emmanuel <kofibusy@gmail.com>
2023-06-19 13:46:47 +0000

    add sluggable trait

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
 
 class Task extends Model
 {
-    use HasFactory, ModelBootingTrait, SoftDeletes;
+    use HasFactory, ModelBootingTrait, SoftDeletes, HasSlug;
 
     protected $guarded = [
         "id",
