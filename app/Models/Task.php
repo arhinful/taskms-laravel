6e21c72e8610428aa58ffdcdcabf023a74529f24
Arhinful Emmanuel <kofibusy@gmail.com>
2023-06-19 12:00:34 +0000

    added guarded and fillable properties on Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,4 +9,16 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory, ModelBootingTrait;
+
+    protected $guarded = [
+        "id",
+        "uuid",
+        "created_at",
+        "deleted_at",
+    ];
+
+    protected $fillable = [
+        "task",
+        "description",
+    ];
 }
