86aa642e125b81e537f555b308ef7f9a37bb52ce
Arhinful Emmanuel <kofibusy@gmail.com>
2023-06-21 19:01:15 +0000

    readme

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,5 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
+        'https://tasksm-frontend.vercel.app',
+        'https://tasksm-frontend.vercel.app/*',
     ];
 }
