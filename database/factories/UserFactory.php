0152d4358fa616492f70a45847fd38b42f78c06f
Arhinful Emmanuel <kofibusy@gmail.com>
2023-06-21 15:46:36 +0000

    modify user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,30 +10,15 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => "Arhinful Emmanuel",
+            'email' => "kofibusy@gmail.com",
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
             'uuid' => Str::uuid()->toString(),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
